01f572d22a194e0db3e4caa007fd93091866e9ed
Alex.Kourov <spam.magaziny@ya.ru>
2020-04-16 13:39:22 +0500

    оптимизирован запрос тэгов

--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -7,7 +7,7 @@ from articles.models import Article, Tags
 def articles_list(request):
     template = 'articles/news.html'
     ordering = '-published_at'
-    articles = Article.objects.prefetch_related('tagrelation_set').order_by(ordering)
+    articles = Article.objects.prefetch_related('tagrelation_set', 'tagrelation_set__tags').order_by(ordering)
 
     context = {'object_list': articles,
               }
